c31f74708000fd184053cce2f57f583135e8c67b
Passarin27 <gsilvasouza116658@gmail.com>
2026-01-31 14:53:04 -0300

    Update users.routes.js

--- Cinematch-Backend/src/routes/users.routes.js
+++ Cinematch-Backend/src/routes/users.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const supabase = require('../config/supabase');
 const authMiddleware = require('../middlewares/auth.middleware');
+const bcrypt = require('bcryptjs');
 
 /* =========================
    GET /users/me
@@ -23,17 +24,18 @@ router.get('/me', authMiddleware, async (req, res) => {
 /* =========================
    PUT /users/me
 ========================= */
-const bcrypt = require('bcryptjs');
-
 router.put('/me', authMiddleware, async (req, res) => {
-  const { nome, email, foto, senha } = req.body;
-
   try {
+    const { nome, email, foto, senha } = req.body;
+
     const dadosAtualizar = {};
 
-    if (nome) dadosAtualizar.nome = nome;
-    if (email) dadosAtualizar.email = email;
-    if (foto) dadosAtualizar.foto = foto;
+    if (nome !== undefined) dadosAtualizar.nome = nome;
+    if (email !== undefined) dadosAtualizar.email = email;
+
+    if (foto !== undefined) {
+      dadosAtualizar.foto = foto;
+    }
 
     if (senha) {
       const senhaHash = await bcrypt.hash(senha, 10);

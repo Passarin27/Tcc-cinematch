a3e7c3e057f2c1e1fccf2761343ac0ef800f936e
Passarin27 <gsilvasouza116658@gmail.com>
2026-02-01 15:16:34 -0300

    Update auth.routes.js

--- Cinematch-Backend/src/routes/auth.routes.js
+++ Cinematch-Backend/src/routes/auth.routes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { authMiddleware } = require('../controllers/auth.controller');
+const authController = require('../controllers/auth.controller');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
 module.exports = router;
-
